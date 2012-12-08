3395b51be5600c71ecef6c9f7e347bcff4b28dbc
Braden <bradenwatling@hotmail.com>
2012-12-07 22:02:17 -0500

    Initial Commit

---
+++ UlamSpiral/Main.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace UlamSpiral
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sizeBox = new System.Windows.Forms.TextBox();
+            this.sizeLabel = new System.Windows.Forms.Label();
+            this.containerPanel = new System.Windows.Forms.Panel();
+            this.viewPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fontList = new System.Windows.Forms.ListBox();
+            this.drawDotsBox = new System.Windows.Forms.CheckBox();
+            this.squareButton = new System.Windows.Forms.RadioButton();
+            this.primeButton = new System.Windows.Forms.RadioButton();
+            this.containerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sizeBox
+            // 
+            this.sizeBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.sizeBox.Location = new System.Drawing.Point(697, 28);
+            this.sizeBox.MaxLength = 3;
+            this.sizeBox.Name = "sizeBox";
+            this.sizeBox.Size = new System.Drawing.Size(38, 20);
+            this.sizeBox.TabIndex = 1;
+            this.sizeBox.TextChanged += new System.EventHandler(this.sizeBox_TextChanged);
+            this.sizeBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.sizeBox_KeyPress);
+            // 
+            // sizeLabel
+            // 
+            this.sizeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.sizeLabel.AutoSize = true;
+            this.sizeLabel.Location = new System.Drawing.Point(697, 12);
+            this.sizeLabel.Name = "sizeLabel";
+            this.sizeLabel.Size = new System.Drawing.Size(30, 13);
+            this.sizeLabel.TabIndex = 2;
+            this.sizeLabel.Text = "Size:";
+            // 
+            // containerPanel
+            // 
+            this.containerPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.containerPanel.AutoScroll = true;
+            this.containerPanel.Controls.Add(this.viewPanel);
+            this.containerPanel.Location = new System.Drawing.Point(12, 12);
+            this.containerPanel.Name = "containerPanel";
+            this.containerPanel.Size = new System.Drawing.Size(679, 419);
+            this.containerPanel.TabIndex = 3;
+            // 
+            // viewPanel
+            // 
+            this.viewPanel.Location = new System.Drawing.Point(218, 131);
+            this.viewPanel.Name = "viewPanel";
+            this.viewPanel.Size = new System.Drawing.Size(200, 100);
+            this.viewPanel.TabIndex = 0;
+            this.viewPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.viewPanel_Paint);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(697, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Font:";
+            // 
+            // fontList
+            // 
+            this.fontList.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fontList.FormattingEnabled = true;
+            this.fontList.Items.AddRange(new object[] {
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.fontList.Location = new System.Drawing.Point(697, 76);
+            this.fontList.Name = "fontList";
+            this.fontList.Size = new System.Drawing.Size(39, 108);
+            this.fontList.TabIndex = 5;
+            this.fontList.SelectedIndexChanged += new System.EventHandler(this.fontList_SelectedIndexChanged);
+            // 
+            // drawDotsBox
+            // 
+            this.drawDotsBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.drawDotsBox.AutoSize = true;
+            this.drawDotsBox.Checked = true;
+            this.drawDotsBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.drawDotsBox.Location = new System.Drawing.Point(697, 190);
+            this.drawDotsBox.Name = "drawDotsBox";
+            this.drawDotsBox.Size = new System.Drawing.Size(48, 17);
+            this.drawDotsBox.TabIndex = 6;
+            this.drawDotsBox.Text = "Dots";
+            this.drawDotsBox.UseVisualStyleBackColor = true;
+            this.drawDotsBox.CheckedChanged += new System.EventHandler(this.drawDotsBox_CheckedChanged);
+            // 
+            // squareButton
+            // 
+            this.squareButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.squareButton.AutoSize = true;
+            this.squareButton.Checked = true;
+            this.squareButton.Location = new System.Drawing.Point(697, 226);
+            this.squareButton.Name = "squareButton";
+            this.squareButton.Size = new System.Drawing.Size(64, 17);
+            this.squareButton.TabIndex = 7;
+            this.squareButton.TabStop = true;
+            this.squareButton.Text = "Squares";
+            this.squareButton.UseVisualStyleBackColor = true;
+            this.squareButton.CheckedChanged += new System.EventHandler(this.squareButton_CheckedChanged);
+            // 
+            // primeButton
+            // 
+            this.primeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.primeButton.AutoSize = true;
+            this.primeButton.Location = new System.Drawing.Point(697, 249);
+            this.primeButton.Name = "primeButton";
+            this.primeButton.Size = new System.Drawing.Size(56, 17);
+            this.primeButton.TabIndex = 8;
+            this.primeButton.Text = "Primes";
+            this.primeButton.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(806, 443);
+            this.Controls.Add(this.primeButton);
+            this.Controls.Add(this.squareButton);
+            this.Controls.Add(this.drawDotsBox);
+            this.Controls.Add(this.fontList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sizeLabel);
+            this.Controls.Add(this.sizeBox);
+            this.Controls.Add(this.containerPanel);
+            this.Name = "Main";
+            this.Text = "Ulam\'s Spiral";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.containerPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sizeBox;
+        private System.Windows.Forms.Label sizeLabel;
+        private System.Windows.Forms.Panel containerPanel;
+        private System.Windows.Forms.Panel viewPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox fontList;
+        private System.Windows.Forms.CheckBox drawDotsBox;
+        private System.Windows.Forms.RadioButton squareButton;
+        private System.Windows.Forms.RadioButton primeButton;
+
+    }
+}
+
---
+++ UlamSpiral/Main.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UlamSpiral
+{
+    public partial class Main : Form
+    {
+        int spiralWidth = 2;
+
+        List<SpiralNumber> spiralNumbers;
+
+        Font drawFont;
+        SolidBrush evenBrush;
+        SolidBrush oddBrush;
+        SolidBrush squareBrush;
+        SolidBrush primeBrush;
+        SolidBrush squarePrimeBrush;
+
+        Size spiralSize;
+        Size totalSpiralSize;
+        Size fontSize;
+
+        SpeechRecognitionEngine speechEngine = new SpeechRecognitionEngine();
+
+        bool drawDots;
+        bool squareCenter;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            fontList.SelectedIndex = 0;
+            squareCenter = squareButton.Checked;
+
+            speechEngine.SpeechRecognized +=new EventHandler<SpeechRecognizedEventArgs>(speechEngine_SpeechRecognized);
+
+            speechEngine.SetInputToDefaultAudioDevice();
+
+            Choices choices = new Choices("primes", "squares", "dots", "numbers");
+
+            foreach(string item in fontList.Items)
+                choices.Add("set font " + item);
+
+            for (int i = 0; i <= 999; ++i)
+                choices.Add("set size " + i);
+
+            GrammarBuilder grammarBuilder = new GrammarBuilder(choices);
+            speechEngine.LoadGrammar(new Grammar(grammarBuilder));
+            speechEngine.RecognizeAsync(RecognizeMode.Multiple);
+
+            init();
+        }
+
+        private void init()
+        {
+            drawDots = drawDotsBox.Checked;
+
+            spiralNumbers = new List<SpiralNumber>();
+
+            drawFont = drawFont == null ? new Font("Times New Roman", 3) : drawFont;
+            evenBrush = new SolidBrush(Color.Black);
+            oddBrush = new SolidBrush(Color.Black);
+            squareBrush = new SolidBrush(Color.Lime);
+            primeBrush = new SolidBrush(Color.Magenta);
+            squarePrimeBrush = new SolidBrush(Color.Cyan);
+
+            spiralSize = new Size(spiralWidth, spiralWidth);
+
+            using (Graphics g = this.CreateGraphics())
+                fontSize = g.MeasureString(" ", drawFont).ToSize();
+
+            containerPanel.Invalidate();
+            viewPanel.Invalidate();
+
+            spiralNumbers = drawSpiral();
+
+            viewPanel.Bounds = new Rectangle(Point.Empty, totalSpiralSize);
+        }
+
+        private void speechEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string text = e.Result.Text;
+
+            if (text.Contains("set font"))
+            {
+                int index = Convert.ToInt32(text.Split(' ')[2]);
+
+                bool validIndex = false;
+
+                foreach (string item in fontList.Items)
+                    if (item == index.ToString())
+                    {
+                        validIndex = true;
+                        break;
+                    }
+                if(validIndex)
+                    fontList.SelectedIndex = index - 3;
+                return;
+            }
+            else if (text.Contains("set size"))
+            {
+                int value = Convert.ToInt32(text.Split(' ')[2]);
+
+                sizeBox.Text = value.ToString();
+                return;
+            }
+
+            switch (text)
+            {
+                case "primes":
+                    primeButton.Checked = true;
+                    break;
+                case "squares":
+                    squareButton.Checked = true;
+                    break;
+                case "dots":
+                    drawDotsBox.Checked = true;
+                    break;
+                case "numbers":
+                    drawDotsBox.Checked = false;
+                    break;
+            }
+        }
+
+        private void viewPanel_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (SpiralNumber spiralNumber in spiralNumbers)
+                spiralNumber.draw(e.Graphics, drawFont, drawDots);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void sizeBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                sizeBox.KeyPress += new KeyPressEventHandler(sizeBox_KeyPress);
+                spiralWidth = Convert.ToInt32(sizeBox.Text);
+
+                init();
+            }
+            catch
+            {
+            }
+        }
+
+        private void sizeBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != '\b') //Only allow numbers
+                e.Handled = true;
+        }
+
+        private void fontList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            drawFont = new Font("Times New Roman", (float)Convert.ToDouble((sender as ListBox).SelectedItem));
+            init();
+        }
+
+        private void drawDotsBox_CheckedChanged(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        private void squareButton_CheckedChanged(object sender, EventArgs e)
+        {
+            squareCenter = squareButton.Checked;
+            init();
+        }
+
+        private List<SpiralNumber> drawSpiral()
+        {
+            int rowWidth = 1, columnHeight = 1;
+            int numNumbers = spiralSize.Width * spiralSize.Height;
+
+            int horizontalSpacing = 2 * numNumbers.ToString().Length; //Compensate for the number of digits
+
+            Size fontWidth = new Size(fontSize.Width * (drawDots ? 1 : horizontalSpacing), 0); //Dont add extra space for dots
+            Size fontHeight = new Size(0, drawDots ? fontSize.Width : fontSize.Height); //Square dots or font height
+
+            totalSpiralSize = new Size(fontWidth.Width * (spiralWidth + 2), fontHeight.Height * (spiralWidth + 2));
+
+            Point pos = new Point(totalSpiralSize.Width / 2, totalSpiralSize.Height / 2);
+            pos -= fontWidth + fontHeight;
+
+            List<SpiralNumber> numbers = new List<SpiralNumber>();
+
+            int numberValue = 1;
+
+            if (!squareCenter)
+            {
+                numberValue = 41;
+                numNumbers += numberValue - 1;
+            }
+
+            numbers.Add(new SpiralNumber(pos, calculateBrush(numberValue), numberValue++));
+
+            while (numberValue <= numNumbers)
+            {
+                //Move right
+                for (int i = 1; i <= rowWidth && numberValue <= numNumbers; ++i)
+                {
+                    pos += fontWidth;
+                    numbers.Add(new SpiralNumber(pos, calculateBrush(numberValue), numberValue++));
+                }
+                ++rowWidth;
+
+                //Move up
+                for (int i = 1; i <= columnHeight && numberValue <= numNumbers; ++i)
+                {
+                    pos -= fontHeight;
+                    numbers.Add(new SpiralNumber(pos, calculateBrush(numberValue), numberValue++));
+                }
+                ++columnHeight;
+
+                //Move left
+                for (int i = 1; i <= rowWidth && numberValue <= numNumbers; ++i)
+                {
+                    pos -= fontWidth;
+                    numbers.Add(new SpiralNumber(pos, calculateBrush(numberValue), numberValue++));
+                }
+                ++rowWidth;
+
+                //Move down
+                for (int i = 1; i <= columnHeight && numberValue<= numNumbers; ++i)
+                {
+                    pos += fontHeight;
+                    numbers.Add(new SpiralNumber(pos, calculateBrush(numberValue), numberValue++));
+                }
+                ++columnHeight;
+            }
+
+            return numbers;
+        }
+
+        private bool isSquare(int num)
+        {
+            double sqrt = Math.Sqrt(num);
+
+            return num % sqrt == 0;
+        }
+
+        private bool isPrime(int num)
+        {
+            if(num == 1 || num == 2) //One and two are special cases
+                return true;
+            if (num % 2 == 0) //Even numbers cant be prime
+                return false;
+
+            for (int i = 3; i <= num / 2; ++i) //We eliminated 1 and 2
+                if (num % i == 0) //If the number is a factor of num
+                    return false;
+
+            return true;
+        }
+
+        private SolidBrush calculateBrush(int num)
+        {
+            SolidBrush brush;
+
+            brush = num % 2 == 0 ? evenBrush : oddBrush;
+            brush = isSquare(num) ? squareBrush : brush;
+            brush = isPrime(num) ? primeBrush : brush;
+
+            if (num == 1)
+                brush = squarePrimeBrush; //1 is the only number that is a square and prime
+
+            return brush;
+        }
+    }
+
+    class SpiralNumber
+    {
+        public Point position;
+        public SolidBrush brush;
+        public int number;
+
+        public SpiralNumber(Point p, SolidBrush b, int n)
+        {
+            position = p;
+            brush = b;
+            number = n;
+        }
+
+        public void draw(Graphics g, Font font, bool drawDots)
+        {
+            if (drawDots)
+            {
+                Size fontSize = g.MeasureString(" ", font).ToSize();
+
+                Rectangle rect = new Rectangle(position.X, position.Y, fontSize.Width, fontSize.Width);
+                g.FillRectangle(brush, rect);
+            }
+            else
+                g.DrawString(number.ToString(), font, brush, position);
+        }
+    }
+}
